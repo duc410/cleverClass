de12aeaa64fb120f6fdef2f9c55ebd37a1f1cf11
LongBody <nguyenthanhlong1120@gmail.com>
2020-02-20 21:50:52 +0700

    up code fb sign in

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function init() {
 
     view.showComponents('loading')
 
-    firebase.auth().onAuthStateChanged(function(user, result) {
+    firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             $('body').css('padding-right', '0px')
             view.showComponents('personal')
