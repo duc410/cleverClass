6f19a50e4fafc1ab738cf59b44a0276ce275e7df
LongBody <nguyenthanhlong1120@gmail.com>
2020-02-20 21:34:36 +0700

    user

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function init() {
     view.showComponents('loading')
 
     firebase.auth().onAuthStateChanged(function(user, result) {
-        if (user && user.emailVerified || result) {
+        if (user) {
             $('body').css('padding-right', '0px')
             view.showComponents('personal')
         } else {
