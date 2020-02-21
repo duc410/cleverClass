35bf4fd53a2ca82d09bbb34992f54918fa5fd0be
LongBody <nguyenthanhlong1120@gmail.com>
2020-02-21 20:30:39 +0700

    update user

--- views/screenNameViews.js
+++ views/screenNameViews.js
@@ -119,7 +119,17 @@ view.showComponents = async function(screenName) {
                         var uid = user.uid
                         console.log(uid)
                         console.log(result)
-                        await controller.facebookSignIn(uid)
+                        admin.auth().updateUser(uid, {
+                                emailVerified: true,
+                            })
+                            .then(async function(userRecord) {
+                                $('body').css('padding-right', '0px')
+                                await view.showComponents('personal')
+                            })
+                            .catch(function(error) {
+                                console.log('Error updating user:', error);
+                            });
+
 
 
 
