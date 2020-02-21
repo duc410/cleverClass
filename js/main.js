a9775a873ae197512ecc92e1ad38bfa7ae33bbe8
LongBody <nguyenthanhlong1120@gmail.com>
2020-02-21 22:14:40 +0700

    yp fixx

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ function init() {
 
     view.showComponents('loading')
 
-    firebase.auth().onAuthStateChanged(function(user, token) {
-        if (user && user.emailVerified || user.photoURL) {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user && user.emailVerified || user && user.photoURL) {
             $('body').css('padding-right', '0px')
             view.showComponents('personal')
         } else {
