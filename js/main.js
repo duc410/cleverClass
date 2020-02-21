150bc2ed72c47832acf6fc9057eddab9ed1b4b8b
LongBody <nguyenthanhlong1120@gmail.com>
2020-02-21 22:16:37 +0700

    update

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function init() {
     view.showComponents('loading')
 
     firebase.auth().onAuthStateChanged(function(user) {
-        if (user && user.emailVerified || user && user.photoURL) {
+        if (user && user.emailVerified || user && user.providerData[0].providerId === "facebook.com") {
             $('body').css('padding-right', '0px')
             view.showComponents('personal')
         } else {
