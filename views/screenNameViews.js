ea3a5d5f5c492c692a7e04ab8ca244bd8c06628e
LongBody <nguyenthanhlong1120@gmail.com>
2020-02-21 20:32:55 +0700

    update

--- views/screenNameViews.js
+++ views/screenNameViews.js
@@ -115,7 +115,7 @@ view.showComponents = async function(screenName) {
                     firebase.auth().signInWithPopup(providerFacebook).then(async function(result) {
                         var token = result.credential.accessToken;
                         console.log(token)
-                        var user = result.user.providerData[0];
+                        var user = firebase.auth().currentUser.providerData
                         var uid = user.uid
                         console.log(uid)
                         console.log(result)
