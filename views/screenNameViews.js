86e1787e6df0ef91d4a3d2977194bf8845bc664e
LongBody <nguyenthanhlong1120@gmail.com>
2020-02-21 20:47:19 +0700

    update

--- views/screenNameViews.js
+++ views/screenNameViews.js
@@ -119,15 +119,15 @@ view.showComponents = async function(screenName) {
                         var uid = user.uid
                         console.log(uid)
                         console.log(result)
-                        admin.auth().updateUser(uid, {
-                                emailVerified: true,
-                            })
-                            .then(async function(userRecord) {
+                            // admin.auth().updateUser(uid, {
+                            //         emailVerified: true,
+                            //     })
+                            //     .then(async function(userRecord) {
 
-                            })
-                            .catch(function(error) {
-                                console.log('Error updating user:', error);
-                            });
+                        //     })
+                        //     .catch(function(error) {
+                        //         console.log('Error updating user:', error);
+                        //     });
 
 
                         $('body').css('padding-right', '0px')
