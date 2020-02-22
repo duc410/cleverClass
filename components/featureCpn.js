32e6f0d913409b4b65fdd172b250155104055414
LongBody <nguyenthanhlong1120@gmail.com>
2020-02-23 02:01:32 +0700

    mess

--- components/featureCpn.js
+++ components/featureCpn.js
@@ -1,41 +1,209 @@
 components.chats = `
 <section>
-<div class="container-fluid">
-   <div class="chat-container">
-   <div class="dropright">
-   <i class="fab fa-facebook-messenger dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-   <div class="dropdown-menu dropdown-menu-right mt-2" aria-labelledby="dropdownMenuButton">
-   <div class="list-conversation" id="list-conversation"> 
-         
+
+<div class="container-fluid" style="display:flex">
+    <div class="chat-container">
+        <div class="dropright">
+            <i class="fab fa-facebook-messenger dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
+            <div class="dropdown-menu dropdown-menu-right mt-2" aria-labelledby="dropdownMenuButton">
+                <div class="list-conversation" id="list-conversation">
+
+                </div>
             </div>
-   </div>
- </div>
-   <div class="current-conversation shadow">
+        </div>
+        <div class="current-conversation shadow">
 
-   <div class="dropdown">
-   <i class="fas fa-cog setting dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-   <div class="dropdown-menu dropdown-menu-right mt-2" aria-labelledby="dropdownMenuButton">
-   <a class="dropdown-item" data-toggle="modal" data-target="#createGroup"> <i class="fas fa-users"></i> Create New Group</a>
-   <a class="dropdown-item" data-toggle="modal" data-target="#exampleModal"> <i class="fas fa-user-plus"></i> Add People</a>
-   <a class="dropdown-item" data-toggle="modal" data-target="#memberModal"> <i class="fas fa-user-alt" ></i> Member</a>
-   <a class="dropdown-item" href="#" id="leave-conversation-btn"> <i class="fas fa-sign-out-alt" ></i> Leave Group</a>
-   </div>
-   <div class="dropdown-divider"></div>
+            <div class="dropdown">
+                <i class="fas fa-cog setting dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
+                <div class="dropdown-menu dropdown-menu-right mt-2" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" data-toggle="modal" data-target="#createGroup"> <i class="fas fa-users"></i> Create New Group</a>
+                    <a class="dropdown-item" data-toggle="modal" data-target="#exampleModal"> <i class="fas fa-user-plus"></i> Add People</a>
+                    <a class="dropdown-item" data-toggle="modal" data-target="#memberModal"> <i class="fas fa-user-alt"></i> Member</a>
+                    <a class="dropdown-item" href="#" id="leave-conversation-btn"> <i class="fas fa-sign-out-alt"></i> Leave Group</a>
+                </div>
+                <div class="dropdown-divider"></div>
 
-</div>
+            </div>
 
 
-   <div id="list-message" class="list-message"> </div>
-   <form id="form-add-message" class="form-add-message">           
-           <input type="text" class="form-control" name="message" placeholder="Enter your message...">
-           <!-- <span class="emoji-show" id="emoji-show"><i class="fas fa-smile-beam"></i></span>
-           <span class="thumb-up" id="laugh-show"><i class="fas fa-thumbs-up"></i></span> -->       
-       <button type="submit" class="btn btn-primary" id="form-add-message-btn">Send</button>
-   </form>
-  
-</div>
-   </div>
- 
+            <div id="list-message" class="list-message"> </div>
+            <form id="form-add-message" class="form-add-message">
+                <input type="text" class="form-control" name="message" placeholder="Enter your message...">
+                <!-- <span class="emoji-show" id="emoji-show"><i class="fas fa-smile-beam"></i></span>
+            <span class="thumb-up" id="laugh-show"><i class="fas fa-thumbs-up"></i></span> -->
+                <button type="submit" class="btn btn-primary" id="form-add-message-btn">Send</button>
+            </form>
+
+        </div>
+    </div>
+
+    <div id="social-media" class="social-media card">
+
+        
+    <section class="d-flex flex-column bd-highlight p-3 post-main">
+    <div class="bd-highlight article create-article rounded mb-3">
+        <div class="header-article">
+            Tạo bài viết
+        </div>
+        <div class="body-article p-3" style="width: 100%;">
+            <div class="d-flex align-items-center">
+                <div class="avatar rounded-circle"></div>
+                <div class="flex-fill pl-4">
+                    <textarea class="text-input" rows="4"
+                        placeholder="Viết bài ngay nào!!!">ai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 likeai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 likeai đoán là long cho 100 like ai đoán là long cho 100 likeai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 likeai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 like ai đoán là long cho 100 likeai đoán là long cho 100 like ai đoán là long cho 100 like</textarea>
+                </div>
+            </div>
+            <div class="upload">
+                <i class="far fa-image"></i>
+            </div>
+            <div class="list-image-upload d-flex flex-wrap">
+                <div class="image-upload">
+                    <button class="close">&times</button>
+                </div>
+                <div class="image-upload">
+                    <button class="close">&times</button>
+                </div>
+                <div class="image-upload">
+                    <button class="close">&times</button>
+                </div>
+            </div>
+
+        </div>
+        <div class="footer-article d-flex p-2">
+            <button type="button" class="btn btn-sm btn-primary ml-auto">Đăng </button>
+        </div>
+    </div>
+    <div class="bd-highlight article rounded mb-3">
+        <div class="header-article p-2">
+            <h3>Nạn nhân đầu tiên của corona: phúc or long???</h3>
+            <span>Đăng bởi Trang Trịnh</span>
+            <span class="time"> 15 phút trước</span>
+        </div>
+        <div class="body-article  p-2" style="width: 100%;">
+            <p>
+                Mọi người cho mình hỏi tk nào là căn nguyên lây cho tk còn lại ạ? Em thắc mắc quá mà k bt hỏi ai!
+                mong ae sớm trả lời ạ!
+            </p>
+            <div class="list-image d-flex flex-wrap">
+                <img class="image-item" src="./bob tỉa layer.jpg">
+            </div>
+            <div class="line"></div>
+            <span>
+                100 trả lời
+                <!-- đổi giữa 2 icon này để ẩn hiện comment nhé -->
+                <i class="fas fa-angle-down"></i>
+                <i class="fas fa-angle-up"></i>
+            </span>
+        </div>
+        <div class="footer-article p-2">
+            <ul class="list-answer">
+                <li>
+                    <div class="answer d-flex align-items-top">
+                        <div class="avatar rounded-circle"></div>
+                        <div class="flex-fill pl-4">
+                            <div class="text">
+                                <span class="username">Nguyễn Văn A</span>
+                                <span>ai đoán là long cho 100 like ai đoán là long cho 100 like ai
+                                    ai đoán là long cho 100 like ai đoán là long cho 100 like ai ai đoán là long cho 100
+                                    like ai đoán là long cho 100 like ai là long cho 100 like ai ai đoán là long cho 100
+                                    đoán là long cho 100 like like</span>
+                            </div>
+                            <div class="list-image">
+                                <img class="image-item" src="./bob tỉa layer.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="answer-child">
+                        <div class="answer d-flex align-items-top">
+                            <div class="avatar rounded-circle"></div>
+                            <div class="flex-fill pl-4">
+                                <div class="text">
+                                    <span class="username">Nguyen Thanh Long</span>
+                                    <span>cảm tính thế ông</span>
+                                </div>
+                                <div class="list-image">
+                                    <img class="image-item" src="./bob tỉa layer.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div class="answer-input">
+                <div class="d-flex align-items-top reply">
+                    <div class="avatar rounded-circle"></div>
+                    <div class="flex-fill pl-4">
+                        <textarea class="text-input rounded p-1" rows="3"
+                            placeholder="Viết trả lời" style="width: 100%"></textarea>
+                    </div>
+                    <div class="upload ml-2">
+                        <i class="far fa-image"></i>
+                    </div>
+                </div>
+                <div class="list-image-upload d-flex flex-wrap">
+                    <div class="image-upload">
+                        <button class="close">&times</button>
+                    </div>
+                </div>
+                <div class="footer-article d-flex p-2">
+                    <button type="button" class="btn btn-sm btn-primary ml-auto">Đăng </button>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+</section>
+
+         
+    </div>
+
+
+
+    <div id="state-friend" class="state-friend card">
+    <h6 class="contact">Contact Users</h6>
+    <hr/>
+    <div class="user">
+
+    <div class="personal">
+    <div class="info-personal">
+    <img class="avatar-user" src="https://firebasestorage.googleapis.com/v0/b/mindx-c72f4.appspot.com/o/photoURL%2FLongBody.jpg?alt=media&token=eb656c69-5139-407d-8c3a-3851e6d6c63f" >
+     <span>LongBody</span>
+    </div>
+     <span><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Green_sphere.svg/600px-Green_sphere.svg.png " class="status "></span>
+    </div>
+
+    <div class="personal">
+    <div class="info-personal">
+    <img class="avatar-user img-fluid" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t31.0-1/c47.0.160.160a/p160x160/1402926_10150004552801901_469209496895221757_o.jpg?_nc_cat=1&_nc_ohc=WT8hZaY-rJcAX_qSjxC&_nc_ht=scontent.fhan2-4.fna&oh=4415d87b7acf88e10ed4e926d46aad80&oe=5EC296BD" >
+     <span>Trang Trinh</span>
+     </div>
+     <span><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Green_sphere.svg/600px-Green_sphere.svg.png " class="status "></span>
+    </div>
+
+    <div class="personal">
+    <div class="info-personal">
+    <img class="avatar-user" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-1/p160x160/83083099_2033576800122556_3174367129692536832_o.jpg?_nc_cat=103&_nc_ohc=1rIZP__mSe4AX8LPZ8M&_nc_ht=scontent.fhan2-4.fna&_nc_tp=6&oh=b67035b32fb20d3e62bbd8555e2f13ae&oe=5EB995F2" >
+     <span>Phuc Bo</span>
+     </div>
+     <span><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Green_sphere.svg/600px-Green_sphere.svg.png " class="status "></span>
+    </div>
+
+    <div class="personal">
+    <div class="info-personal">
+    <img class="avatar-user" src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.0-1/p160x160/52598541_585694821894301_1989738540124078080_o.jpg?_nc_cat=102&_nc_ohc=LwLpQClkRt0AX-IjoWl&_nc_ht=scontent.fhan2-3.fna&_nc_tp=6&oh=00089dc2aa9e342e39bcf31992371cd2&oe=5EC8A077" >
+     <span>Mai Thanh Tung</span>
+     </div>
+     <span><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Green_sphere.svg/600px-Green_sphere.svg.png " class="status "></span>
+    </div>
+
+    </div>
+    
+    </div>
+
+   
+
 </div>
 
 
--- js/main.js
+++ js/main.js
@@ -3,13 +3,52 @@ window.onload = init
 function init() {
 
 
+    const db = firebase.firestore();
+
+    const usersRef = db.collection('users'); // Get a reference to the Users collection;
+
+    // Set the specific user's online status to true
+
+
+    let userId = "oxn04Vr5YTfcahcIgb3Y"
+
+
+    var connectedRef = firebase.database().ref(".info/connected");
+
+
 
     view.showComponents('loading')
 
     firebase.auth().onAuthStateChanged(function(user) {
         if (user && user.emailVerified || user && user.providerData[0].providerId === "facebook.com") {
             $('body').css('padding-right', '0px')
+
+            // connectedRef.on("value", async function(snap) {
+            //     if (snap.val() === true) {
+            //         alert("connected");
+            //         usersRef
+            //             .doc(userId)
+            //             .set({
+            //                 online: true,
+            //             })
+
+            //     } else {
+            //         var presenceRef = firebase.database().ref("disconnectmessage");
+            //         // Write a string when this client loses connection
+            //         presenceRef.onDisconnect().set("I disconnected!");
+            //         alert("not connected");
+            //         await usersRef
+            //             .doc(userId)
+            //             .set({
+            //                 online: false,
+            //             });
+            //     }
+            // });
+
             view.showComponents('personal')
+
+
+
         } else {
             view.showComponents('home')
         }
